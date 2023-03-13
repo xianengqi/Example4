50cdb05200cd5acde4bad60cba062884a437483a
XiaNengQi <920986617@qq.com>
2023-03-13 20:44:19 +0800

    Initial Commit

---
+++ Example4/Example4App.swift
@@ -0,0 +1,20 @@
+//
+//  Example4App.swift
+//  Example4
+//
+//  Created by 夏能啟 on 2023/3/13.
+//
+
+import SwiftUI
+
+@main
+struct Example4App: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
