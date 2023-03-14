9c171e8b671746ed2d35b0f4e7a0fc775037a5af
XiaNengQi <920986617@qq.com>
2023-03-14 19:54:15 +0800

    Update SkuDetailView.swift

--- Example4/SkuDetailView.swift
+++ Example4/SkuDetailView.swift
@@ -11,24 +11,56 @@ struct SkuDetailView: View {
     }
   }
 
+  @State private var quantityToAdd: String = ""
+  @State private var quantityToRemove: String = ""
+
   var body: some View {
     VStack {
       Text(sku.color ?? "" + " " + (sku.size ?? ""))
       Text("库存: \(stock)")
+
+      HStack {
+        TextField("入库", text: $quantityToAdd, onCommit: {
+          if let quantity = Int16(quantityToAdd) {
+            stock += quantity
+            quantityToAdd = ""
+          }
+        })
+        .textFieldStyle(RoundedBorderTextFieldStyle())
+        .frame(width: 100)
+
+        TextField("出库", text: $quantityToRemove, onCommit: {
+          if let quantity = Int16(quantityToRemove) {
+            stock = max(stock - quantity, 0)
+            quantityToRemove = ""
+          }
+        })
+        .textFieldStyle(RoundedBorderTextFieldStyle())
+        .frame(width: 100)
+      }
+
       HStack {
         Button("入库") {
-          stock += 1
+//          stock += 1
+          if let quantity = Int16(quantityToAdd) {
+            stock += quantity
+            quantityToAdd = ""
+          }
         }
         .buttonStyle(BorderlessButtonStyle())
 
         Button("出库") {
-          stock = max(stock - 1, 0)
+//          stock = max(stock - 1, 0)
+          if let quantity = Int16(quantityToRemove) {
+            stock = max(stock - quantity, 0)
+            quantityToRemove = ""
+          }
         }
         .buttonStyle(BorderlessButtonStyle())
       }
       .frame(maxWidth: 100)
     }
-  
+
     .onAppear {
       stock = sku.stock
     }
